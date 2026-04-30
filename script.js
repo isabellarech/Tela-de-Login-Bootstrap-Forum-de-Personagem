48776729ade93c852e9adcc8d220c931a49f7dc3
sophia <sophia_hardt@estudante.sesisenai.org.br>
2026-04-30 11:16:56 -0300

    ADD - fiz algumas validações

---
+++ script.js
@@ -0,0 +1,19 @@
+let usuarioFixo = "Admin";
+let senhaFixa = "54321";
+
+document.getElementById("form-login").onsubmit = (e) => {
+    e.preventDefault();
+
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+    
+    if (usuario !== usuarioFixo) {
+        alert("Usuário incorreto!");
+        return;
+    }
+
+    if (senha !== senhaFixa) {
+        alert("Senha incorreta!");
+        return;
+    }
+}
